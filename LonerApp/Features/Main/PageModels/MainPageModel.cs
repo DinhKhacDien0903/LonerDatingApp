4b9be4c6324056344c45506c93f8ea4e72d7d8ad
diendk <diendk@newwave.vn>
2025-04-10 00:38:30 +0700

    update ipconfig (TODO: move to Enviroment && Add service to call API)

--- LonerApp/Features/Main/PageModels/MainPageModel.cs
+++ LonerApp/Features/Main/PageModels/MainPageModel.cs
@@ -48,18 +48,22 @@ namespace LonerApp.PageModels
         {
             var handler = new HttpClientHandler
             {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true // Bỏ qua lỗi chứng chỉ
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
             };
+            //192.168.1.2
+            //var httpClient = new HttpClient(handler);
 
-            var httpClient = new HttpClient(handler);
-            var baseUrl = DeviceInfo.Platform == DevicePlatform.Android
-                            ? "https://10.0.2.2:7072"
-                            : "https://localhost:7072";
-
-            var response = await httpClient.GetAsync($"{baseUrl}/api/User/getInfor");
-
+            //var baseUrl = DeviceInfo.Platform == DevicePlatform.Android
+            //                ? "https://10.0.2.2:7072"
+            //                : "https://localhost:7072";
+            var _httpClient = new HttpClient(handler);
+            _httpClient.BaseAddress = new Uri("https://192.168.43.14:7072/");
+            var userId = "24150eab-05db-4bc3-b33e-084026aaa380";
+            var request = new HttpRequestMessage(HttpMethod.Get,$"api/User/getInfor?userId={userId}");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
             var data = await response.Content.ReadAsStringAsync();
-            System.Diagnostics.Trace.WriteLine(data);
+            Console.WriteLine(data);
         }
     }
 }
