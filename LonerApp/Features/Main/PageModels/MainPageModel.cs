57fe0772b794b0f205fce9510217d201bde32eb4
diendk <dinhkhacdien1009@gmail.com>
2025-04-09 18:28:00 +0700

    demo call api

--- LonerApp/Features/Main/PageModels/MainPageModel.cs
+++ LonerApp/Features/Main/PageModels/MainPageModel.cs
@@ -21,13 +21,45 @@ namespace LonerApp.PageModels
         {
             //await NavigationService.PushToPageAsync<EmailAuthor>(isPushModal: true);
             //await NavigationService.PushToPageAsync<MainSwipePage>(isPushModal: false);
-            App.RefreshApp();
+            try
+            {
+                // var http = new HttpClient();
+                // string BaseAddress =
+                //    DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
+                // var response = await http.GetAsync("http://10.0.2.2:5250/api/User/getInfor");
+
+                // var data = await response.Content.ReadAsStringAsync();
+                await OnCallApiBtnClicked();
+                App.RefreshApp();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         [RelayCommand]
         async Task OnPhoneSignInAsync(object param)
         {
             await NavigationService.PushToPageAsync<PhoneNumberAuthor>(isPushModal: true);
+        }
+
+        private async Task OnCallApiBtnClicked()
+        {
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true // Bỏ qua lỗi chứng chỉ
+            };
+
+            var httpClient = new HttpClient(handler);
+            var baseUrl = DeviceInfo.Platform == DevicePlatform.Android
+                            ? "https://10.0.2.2:7072"
+                            : "https://localhost:7072";
+
+            var response = await httpClient.GetAsync($"{baseUrl}/api/User/getInfor");
+
+            var data = await response.Content.ReadAsStringAsync();
+            System.Diagnostics.Trace.WriteLine(data);
         }
     }
 }
